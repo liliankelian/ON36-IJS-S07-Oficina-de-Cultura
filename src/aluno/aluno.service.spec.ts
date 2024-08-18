b39a6970d703d8135d8484376907f840a018d88d
lilikelian <lilielili@gmail.com>
2024-08-18 12:28:01 -0300

    Merge com a versao da profa

--- src/aluno/aluno.service.spec.ts
+++ src/aluno/aluno.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlunoService } from './aluno.service';
+import { CreateAlunoDto } from './dto/create-aluno.dto';
+import { ForbiddenException } from '@nestjs/common';
 
 describe('AlunoService', () => {
   let service: AlunoService;
@@ -12,7 +14,22 @@ describe('AlunoService', () => {
     service = module.get<AlunoService>(AlunoService);
   });
 
-  it('should be defined', () => {
+  it('deve estar definido', () => {
     expect(service).toBeDefined();
+  });
+
+  it('deve retornar uma mensagem para o controller"', () => {
+    const aluno = new CreateAlunoDto();
+
+    expect(service.cadastrar(aluno)).toBe(
+      'Eu sou capaz de criar um aluno, mas não sei como salvar essa informação...',
+    );
+  });
+
+  it('should throw ForbiddenException if the age is less than or equal to 16', () => {
+    const aluno = new CreateAlunoDto();
+    aluno.anoNascimento = new Date().getFullYear() - 16;
+
+    expect(() => service.cadastrar(aluno)).toThrow(ForbiddenException);
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,19 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('deve retornar uma mensagem de health check quando a rota / for requisitada', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Olá Programadora!!');
+      .expect('Hello Reprograma!');
+  });
+
+  it('deve retornar uma mensagem quando a rota /alunos for requisitada', () => {
+    return request(app.getHttpServer())
+      .post('/alunos')
+      .expect(201)
+      .expect(
+        'Eu sou capaz de criar um aluno, mas não sei como salvar essa informação...',
+      );
   });
 });
